9fc92b8a5a5badc339b9db17688f1f298f8487cd
Ray Sinnema <rssinnema@yahoo.com>
2015-08-23 13:23:05 +0200

    Text on building REST clients

---
+++ src/test/java/com/securesoftwaredev/restbook/client/ResolvingUrisTest.java
@@ -0,0 +1,23 @@
+package com.securesoftwaredev.restbook.client;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URI;
+
+import org.junit.Test;
+
+
+public class ResolvingUrisTest {
+
+  @Test
+  public void resolve() {
+    URI baseUri = URI.create("http://www.example.com/foo/bar/");
+    URI resolvedUri = baseUri.resolve("baz/");
+    assertEquals(URI.create("http://www.example.com/foo/bar/baz/"), resolvedUri);
+
+    baseUri = URI.create("http://www.example.com/foo/bar");
+    resolvedUri = baseUri.resolve("baz/");
+    assertEquals(URI.create("http://www.example.com/foo/baz/"), resolvedUri);
+  }
+  
+}
