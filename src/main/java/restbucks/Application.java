ea341ca68e4a6ef35a4f44206462b6f7f94889cb
Ray Sinnema <rssinnema@yahoo.com>
2015-10-19 07:07:50 +0200

    Latest RADL

--- src/main/java/restbucks/Application.java
+++ src/main/java/restbucks/Application.java
@@ -6,12 +6,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
 import restbucks.domain.menu.Drink;
 import restbucks.domain.menu.DrinkRepository;
 
 
 @SpringBootApplication
+@EnableHypermediaSupport(type = HypermediaType.HAL)
 public class Application {
 
   public static void main(String[] args) {
